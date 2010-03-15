ab2f090723b2d9068ac3a7fc9cfd476eb479b453
Mark McGranaghan <mmcgrana@gmail.com>
2010-03-15 10:28:17 -0700

    Implement coercion to and from Java Dates.

--- src/clj_time/coerce.clj
+++ src/clj_time/coerce.clj
@@ -8,7 +8,8 @@
      => (from-long 893462400000)
      #<DateTime 1998-04-25T00:00:00.000Z>"
   (:use clj-time.core)
-  (:import (org.joda.time DateTime DateTimeZone)))
+  (:import (org.joda.time DateTime DateTimeZone)
+           java.util.Date))
 
 (defn to-long
   "Returns the number of milliseconds that the given DateTime is after Unix
@@ -21,3 +22,14 @@
    number of milliseconds after the Unix epoch."
    [#^Long millis]
    (DateTime. millis #^DateTimeZone utc))
+
+(defn to-date
+  "Returns a Java Date object corresponding to the given DateTime instance."
+  [#^DateTime dt]
+  (Date. (to-long dt)))
+
+(defn from-date
+  "Returns a DateTime instance in the UTC time zone corresponding to the given
+   Java Date object."
+  [#^Date date]
+  (from-long (.getTime date)))
--- test/clj_time/coerce_test.clj
+++ test/clj_time/coerce_test.clj
@@ -1,9 +1,16 @@
 (ns clj-time.coerce-test
   (:use clojure.test
-        (clj-time core coerce)))
+        (clj-time core coerce))
+  (:import java.util.Date))
 
 (deftest test-to-long
   (is (= 893462400000 (to-long (date-time 1998 4 25)))))
 
 (deftest test-from-long
   (is (= (date-time 1998 4 25) (from-long 893462400000))))
+
+(deftest test-to-from-date
+  (let [dt (from-long 893462400000)
+        d  (to-date dt)]
+    (is (instance? Date d))
+    (is (= dt (from-date d)))))
